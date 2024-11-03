c17ddbf98848c5c39815571ddd7e2fb1e9b21ead
supriya <angalwarsupriya@gmail.com>
2024-11-03 10:13:57 +0530

    changed vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
-/*import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
-*/
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this matches your publish directory in Netlify
-    rollupOptions: {
-      input: './src/main.jsx', // Entry point
-    },
+    outDir: 'dist',
   },
+  base: './',
 });
